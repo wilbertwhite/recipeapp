c98baf5e5456d5bc84309713fb2851b53e28137d
wilbertwhite <wilbertcwhite@gmail.com>
2022-04-09 00:32:22 -0400

    initial commit 3

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ load_dotenv(find_dotenv())
 
 app = flask.Flask(__name__)
 
-app = flask.Flask(__name__)
 db_url = os.getenv("DATABASE_URL")
 if db_url.startswith("postgres://"):
     db_url = db_url.replace("postgres://", "postgresql://", 1)
